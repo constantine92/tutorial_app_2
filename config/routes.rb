7af23d744663853f55046da040613cc09a31e6f1
Gabriel Su <gabrielsumiren@gmail.com>
2016-11-21 18:20:37 +0000

    added user model and controller for user and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'users/show'
+
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
   
