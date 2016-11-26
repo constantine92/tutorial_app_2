250dccd58d1ee8ce1514a45d0daf87f35b27d667
Gabriel Su <gabrielsumiren@gmail.com>
2016-11-26 21:07:25 +0000

    Added flash message

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/about', to: "pages#about"
   get '/contact', to: "pages#contact"
   get '/signup', to: "users#new"
+  post '/signup', to: "users#create"
   get 'users/show'
   get 'users/new'
 
--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -5,7 +5,7 @@ class UserSignupTest < ActionDispatch::IntegrationTest
   
     get signup_path
     assert_no_difference "User.count" do
-      post users_path, params: { user: {  username: "",
+      post signup_path, params: { user: {  username: "",
                                           email: "foobar",
                                           password: "duke",
                                           password_confirmation: "doodoo"
@@ -13,5 +13,21 @@ class UserSignupTest < ActionDispatch::IntegrationTest
                                 }
     end
     assert_template 'users/new'
+    assert_select 'form[action="/signup"]'
   end
+  
+  test "valid submission" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post signup_path, params: { user: { username: "example",
+                                          email: "example@example.com",
+                                          password: "password",
+                                          password_confirmation: "password"
+                                        }
+                                }
+    end
+    follow_redirect!
+    assert_template "users/show"
+  end
+  
 end
