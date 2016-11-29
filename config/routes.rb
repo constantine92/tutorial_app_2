85e30222e76c7948de07eadc166b4e7ba35211c4
Gabriel Su <gabrielsumiren@gmail.com>
2016-11-29 21:06:22 +0000

    Implemented basic login feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get '/login', to: "session#new"
+  post '/login', to: "session#create"
+  delete '/logout', to: "session#destroy"
+
   get '/about', to: "pages#about"
   get '/contact', to: "pages#contact"
   get '/signup', to: "users#new"
