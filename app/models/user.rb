1a6784c2fa2693d38bb7ca2548499bac22957159
Gabriel Su <gabrielsumiren@gmail.com>
2016-12-04 15:41:18 +0000

    added cookies functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   
+  attr_accessor :remember_token
+  
   before_save { self.email = email.downcase }
   VALID_REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, 
@@ -20,6 +22,26 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
     
+  end
+  
+  #create a new token
+ def User.new_token
+  SecureRandom.urlsafe_base64
+ end
+ 
+ #remember user for persistent session
+ def remember
+  self.remember_token = User.new_token
+  update_attribute(:remember_digest, User.digest(remember_token))
+ end
+  
+  def forget
+    update_attribute(:remember_digest,nil)
+  end
+  
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
 end
